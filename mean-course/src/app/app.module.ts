6804141fe752315609801206d71b5f5a83051378
Haakon <haakon.reppen@gmail.com>
2022-10-03 23:27:02 +0200

    Add header for website

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule } from '@angular/material/input'
-import { MatCardModule } from '@angular/material/card'
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +25,8 @@ import { PostCreateComponent } from "./posts/post-create/post-create.component";
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]

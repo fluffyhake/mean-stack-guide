47dd4e3900986c3123f5fbd1bdf5ce1acd439155
Haakon <haakon.reppen@gmail.com>
2022-10-03 15:17:51 +0200

    add styling from material design

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input'
+import { MatCardModule } from '@angular/material/card'
+import { MatButtonModule } from "@angular/material/button";
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
+
 
 
 @NgModule({
@@ -11,7 +17,12 @@ import { PostCreateComponent } from "./posts/post-create/post-create.component";
     PostCreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]

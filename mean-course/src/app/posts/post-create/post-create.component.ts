1c45b1bfa4ec1bb333ac0479e117c2b0ee524e39
Haakon Reppen <haakon.reppen@nhn.no>
2022-11-03 14:41:50 +0100

    Completed with Section 2. Added styiling and eventlisteners. Code cleanup

--- mean-course/src/app/posts/post-create/post-create.component.ts
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -23,5 +23,6 @@ export class PostCreateComponent {
       return;
     }
     this.postService.addPost(form.value.title, form.value.content);
+    form.resetForm();
   }
 }
--- mean-course/src/app/posts/post-list/post-list.component.ts
+++ mean-course/src/app/posts/post-list/post-list.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { Post } from "../post.model";
 import { PostsService } from "../posts.service";
-
+import { Subscription } from "rxjs";
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent implements OnInit {
+export class PostListComponent implements OnInit, OnDestroy {
   // posts = [
   //   {title: 'First Post', content: 'This is the first post content'},
   //   {title: 'Second Post', content: 'This is the second post content'},
@@ -16,10 +16,18 @@ export class PostListComponent implements OnInit {
   //   {title: 'sus', content: 'amogis'}
   // ];
   posts: Post[] = [];
+  private postsSub: Subscription;
 
   constructor(public postsService: PostsService) {
   }
   ngOnInit(){
     this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      })
+  }
+  ngOnDestroy() {
+    this.postsSub.unsubscribe()
   }
 }

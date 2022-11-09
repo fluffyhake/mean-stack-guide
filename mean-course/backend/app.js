c46f36327b509e58b4e05048f2dd521ed475bb41
fluffyhake <haakon.reppen@gmail.com>
2022-11-09 13:49:59 +0100

    Add endpoint for deleting objects. Edit accepted headers and return postID to addPost endpoint

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -20,12 +20,15 @@ mongoose.connect("mongodb://postsdbapp:iDcCQndWwPPh@localhost:27017/postsdb?retr
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers',
-                'Origin, X-Requested-With, Content-Type, Accept');
-
-  res.setHeader('Access-Control-Allow-Methods',
-                'GET, POST, PATCH, DELETE, OPTIONS');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
   next();
 });
 
@@ -34,10 +37,13 @@ app.post("/api/posts", (req,res, next) => {
     title: req.body.title,
     content: req.body.content
   });
-  post.save();
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: 'Post added successfully',
+      postId: createdPost._id
+  });
   console.log(post);
-  res.status(201).json({
-    message: 'Post added successfully'
+
   })
 });
 
@@ -58,6 +64,18 @@ app.get('/api/posts', (req, res, next) => {
   // Responds with 200 OK
 
 });
+
+// ALLOW Paths with ID-s after posts when deleting:
+app.delete("/api/posts/:id", (req, res, next) =>{
+  console.log(req.params.id);
+  Post.deleteOne({_id: req.params.id}).then(result => {
+    console.log(result);
+    res.status(200).json({ message: "Post deleted!" });
+
+  })
+});
+
+
 
 
 module.exports = app;

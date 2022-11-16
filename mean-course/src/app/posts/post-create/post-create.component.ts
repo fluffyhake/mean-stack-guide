d715013a5288c60e1fadf30e067fbddb4580be47
fluffyhake <haakon.reppen@gmail.com>
2022-11-16 10:16:24 +0100

    Add editing logic for the post-create.component.ts

--- mean-course/src/app/posts/post-create/post-create.component.ts
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,9 @@
-import { Component} from "@angular/core";
+import { NgFor } from "@angular/common";
+import { Component, OnInit} from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { ActivatedRoute } from "@angular/router";
+import { ParamMap } from "@angular/router";
+import { Post } from "../post.model";
 
 import { PostsService } from "../posts.service";
 
@@ -9,14 +13,37 @@ import { PostsService } from "../posts.service";
   templateUrl: './post-create.component.html',
   styleUrls: ['./post-create.component.css']
 })
-export class PostCreateComponent {
+export class PostCreateComponent implements OnInit {
   enteredTitle ='';
   enteredContent = '';
+ // Used in the html:
+  post: Post;
+
+  private mode = 'create';
+  private postId: string;
 
 
-  constructor(public postService: PostsService) {}
+  constructor(public postService: PostsService, public route: ActivatedRoute) {}
+
+  ngOnInit(){
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has('postId')) {
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
+        this.post = this.postService.getPost(this.postId);
+      }else{
+        this.mode = 'create';
+        this.postId = null;
+      }
+
+    });
+  }
+
+
 
   newPost = 'NO CONTENT';
+
+
 
   onAddPost(form: NgForm) {
     if(form.invalid){
@@ -24,5 +51,18 @@ export class PostCreateComponent {
     }
     this.postService.addPost(form.value.title, form.value.content);
     form.resetForm();
+  }
+  onSavePost(form: NgForm) {
+    if(form.invalid){
+      return;
+    }
+    if (this.mode === 'create'){
+      this.postService.addPost(form.value.title, form.value.content);
+    } else{
+      this.postService.updatePost(
+        this.postId,
+        form.value.title,
+        form.value.content)
+    }
   }
 }

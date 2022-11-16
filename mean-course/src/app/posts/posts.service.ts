67d72b64a67e6b7e68eab7843ec7516e38a8a1f6
fluffyhake <haakon.reppen@gmail.com>
2022-11-16 12:07:30 +0100

    create function that calls the backend function for reading a specific post

--- mean-course/src/app/posts/posts.service.ts
+++ mean-course/src/app/posts/posts.service.ts
@@ -43,7 +43,7 @@ export class PostsService {
   }
 
   getPost(id: string){
-    return{...this.posts.find(p => p.id === id)}
+    return this.http.get<{_id: string; title: string; content: string}>('http://localhost:3000/api/posts/' + id)
   }
 
 
@@ -63,6 +63,7 @@ export class PostsService {
       .subscribe(response => {
         const updatedPosts = [...this.posts]
         const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id)
+        updatedPosts[oldPostIndex] = post;
         this.posts = updatedPosts;
         this.postsUpdated.next([...this.posts])
       })

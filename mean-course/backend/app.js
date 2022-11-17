c1d19fe2f8b22747be9a5dd63cac2461d5570fcf
fluffyhake <haakon.reppen@gmail.com>
2022-11-17 15:18:03 +0100

    Move routing to seperate file for posts routes

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
+const postsRoutes = require("./routes/posts");
+
+
 // Use big letter first to indicate that this is an object based on a blueprint
 // Import mongodb post schema
-const Post = require("./models/post")
 
 const app = express();
 
@@ -32,71 +34,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/api/posts", (req,res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-  post.save().then(createdPost => {
-    res.status(201).json({
-      message: 'Post added successfully',
-      postId: createdPost._id
-  });
-  console.log(post);
-
-  })
-});
-
-
-// First paramater spcifies the uri
-app.get('/api/posts', (req, res, next) => {
-  // https://mongoosejs.com/docs/queries.html
-  Post.find()
-    .then(documents => {
-      res.status(200).json({
-        message: 'Posts fetched successfully!',
-        posts: documents
-      });
-
-
-
-    });
-  // Responds with 200 OK
-
-});
-
-app.put("/api/posts/:id", (req, res, next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content
-  })
-  Post.updateOne({_id: req.params.id}, post).then(result =>{
-    console.log(result)
-    res.status(200).json({ message : "Update sucsessful!"})
-  })
-});
-
-app.get("/api/posts/:id", (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    if (post) {
-      res.status(200).json(post);
-    }else {
-      res.status(404).json({message: "Post not found!"});
-    }
-  });
-})
-
-// ALLOW Paths with ID-s after posts when deleting:
-app.delete("/api/posts/:id", (req, res, next) =>{
-  console.log(req.params.id);
-  Post.deleteOne({_id: req.params.id}).then(result => {
-    console.log(result);
-    res.status(200).json({ message: "Post deleted!" });
-
-  })
-});
-
+// only paths starting with /api/posts will be forwarded to posts Routes
+app.use("/api/posts", postsRoutes)
 
 
 

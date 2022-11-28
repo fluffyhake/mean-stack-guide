269aceda50b73032563f9a58b888cb1b2bca6ceb
fluffyhake <haakon.reppen@gmail.com>
2022-11-28 08:37:53 +0100

    Add isLoading and update it accordingly

--- mean-course/src/app/posts/post-list/post-list.component.ts
+++ mean-course/src/app/posts/post-list/post-list.component.ts
@@ -15,15 +15,20 @@ export class PostListComponent implements OnInit, OnDestroy {
   //   {title: 'Third Post', content: 'This is the third post content'},
   //   {title: 'sus', content: 'amogis'}
   // ];
+
+  isLoading = false
+
   posts: Post[] = [];
   private postsSub: Subscription;
 
   constructor(public postsService: PostsService) {
   }
   ngOnInit(){
+    this.isLoading = true;
     this.postsService.getPosts();
     this.postsSub = this.postsService.getPostUpdateListener()
       .subscribe((posts: Post[]) => {
+        this.isLoading = false;
         this.posts = posts;
       })
   }

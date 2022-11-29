4a37d232d84fdf6674faf96f3e9f661a12f873ea
fluffyhake <haakon.reppen@gmail.com>
2022-11-29 08:07:37 +0100

    Add backend validation for fileype, update logic to support images

--- mean-course/backend/routes/posts.js
+++ mean-course/backend/routes/posts.js
@@ -1,19 +1,52 @@
 const express = require("express");
 const Post = require("../models/post")
-
+const multer = require("multer");
+const { create } = require("../models/post");
 
 const router = express.Router()
 
+const MIME_TYPE_MAP ={
+  'image/png' : 'png',
+  'image/jpeg' : 'jpg',
+  'image/jpg' : 'jpg'
+}
 
-router.post("", (req,res, next) => {
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error('Invalid mime type');
+    if (isValid){
+      error = null;
+    }
+    // Is relative to where the server.js file is stored
+    cb(error, "backend/images");
+  },
+  filename: (req, file, cb) =>{
+    const name = file.originalname.toLowerCase().split(' ').join('-')
+    const ext = MIME_TYPE_MAP[file.mimetype];
+    cb(null, name + '-' +Date.now() + '.' +ext);
+
+  }
+
+});
+
+router.post("", multer({storage: storage}).single("image"), (req,res, next) => {
+  const url = req.protocol + '://' + req.get("host");
   const post = new Post({
     title: req.body.title,
-    content: req.body.content
+    content: req.body.content,
+    imagePath: url +"/images/" + req.file.filename
   });
   post.save().then(createdPost => {
     res.status(201).json({
       message: 'Post added successfully',
-      postId: createdPost._id
+
+      post: {
+        id: createdPost._id,
+        title: createdPost.title,
+        content: createdPost.content,
+        imagePath: createdPost.imagePath
+      }
   });
   console.log(post);
 
@@ -38,12 +71,19 @@ router.get('', (req, res, next) => {
 
 });
 
-router.put("/:id", (req, res, next) => {
+router.put("/:id", multer({storage: storage}).single("image"), (req, res, next) => {
+  let imagePath = req.body.imagePath;
+  if (req.file) {
+    const url = req.protocol + '://' + req.get("host");
+    imagePath = url +"/images/" + req.file.filename
+  }
   const post = new Post({
     _id: req.body.id,
     title: req.body.title,
-    content: req.body.content
-  })
+    content: req.body.content,
+    imagePath: imagePath
+  });
+  console.log(post);
   Post.updateOne({_id: req.params.id}, post).then(result =>{
     console.log(result)
     res.status(200).json({ message : "Update sucsessful!"})

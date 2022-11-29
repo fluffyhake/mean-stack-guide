c7070005c5bca5166c2aa0e31f275a3d356ffa9e
fluffyhake <haakon.reppen@gmail.com>
2022-11-29 08:21:33 +0100

    Add support for images

--- mean-course/src/app/posts/post-create/post-create.component.ts
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -7,6 +7,8 @@ import { Post } from "../post.model";
 
 import { PostsService } from "../posts.service";
 
+import { mimeType } from "./mime-type.validator"
+
 
 @Component({
   selector: 'app-post-create',
@@ -23,6 +25,7 @@ export class PostCreateComponent implements OnInit {
   private postId: string;
   // Create form and store it in a property. Top level object, groups all the controls of the form.
   form: FormGroup;
+  imagePreview: string;
 
 
   constructor(public postService: PostsService, public route: ActivatedRoute) {}
@@ -36,7 +39,13 @@ export class PostCreateComponent implements OnInit {
       }),
       'content': new FormControl(null, {
         validators: [Validators.required]
-      })
+      }),
+      'image' : new FormControl(null, {
+        validators: [Validators.required],
+        asyncValidators: [mimeType]
+      }
+      )
+
 
 
 
@@ -48,10 +57,15 @@ export class PostCreateComponent implements OnInit {
         this.isLoading = true;
         this.postService.getPost(this.postId).subscribe(postData => {
           this.isLoading = false;
-          this.post = {id: postData._id, title: postData.title, content: postData.content};
+          this.post = {
+            id: postData._id,
+            title: postData.title,
+            content: postData.content,
+            imagePath: postData.imagePath};
           this.form.setValue({
             'title' : this.post.title,
-            'content' : this.post.content
+            'content' : this.post.content,
+            'image' : this.post.imagePath
           });
         });
       }else{
@@ -72,21 +86,46 @@ export class PostCreateComponent implements OnInit {
     if(form.invalid){
       return;
     }
-    this.postService.addPost(form.value.title, form.value.content);
+    this.postService.addPost(this.form.value.title, this.form.value.content, this.form.value.image);
     form.resetForm();
   }
+
+  onImagePicked(event: Event){
+    // Type conversion to tell typescript that event.target is a HTML element
+    const file = (event.target as HTMLInputElement).files[0];
+    // Targets a single control in the form
+    this.form.patchValue({image: file});
+    // Validate the input
+    this.form.get('image').updateValueAndValidity();
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      // Try this with "reader.result as string" if it presents an error
+      this.imagePreview = reader.result as string;
+    };
+    reader.readAsDataURL(file);
+
+    console.log(file);
+    console.log(this.form);
+
+
+  }
+
+
   onSavePost() {
     if(this.form.invalid){
       return;
     }
     this.isLoading = true;
     if (this.mode === 'create'){
-      this.postService.addPost(this.form.value.title, this.form.value.content);
+      this.postService.addPost(this.form.value.title, this.form.value.content, this.form.value.image);
     } else{
       this.postService.updatePost(
         this.postId,
         this.form.value.title,
-        this.form.value.content)
+        this.form.value.content,
+        this.form.value.image
+        )
     }
     this.form.reset();
   }

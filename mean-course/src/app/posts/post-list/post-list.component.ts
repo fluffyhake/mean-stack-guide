a2d13d927ae5022a8380b1b9b0afae7e1f5953f7
fluffyhake <haakon.reppen@gmail.com>
2022-12-02 11:14:25 +0100

    Add the support needed for pagination

--- mean-course/src/app/posts/post-list/post-list.component.ts
+++ mean-course/src/app/posts/post-list/post-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from "@angular/core";
 import { Post } from "../post.model";
 import { PostsService } from "../posts.service";
 import { Subscription } from "rxjs";
+import { PageEvent } from "@angular/material/paginator";
 
 @Component({
   selector: 'app-post-list',
@@ -17,6 +18,11 @@ export class PostListComponent implements OnInit, OnDestroy {
   // ];
 
   isLoading = false
+  // For the paginator
+  totalPosts = 0;
+  postsPerPage = 2;
+  currentPage = 1;
+  pageSizeOptions = [1, 2, 5, 10];
 
   posts: Post[] = [];
   private postsSub: Subscription;
@@ -25,15 +31,26 @@ export class PostListComponent implements OnInit, OnDestroy {
   }
   ngOnInit(){
     this.isLoading = true;
-    this.postsService.getPosts();
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
     this.postsSub = this.postsService.getPostUpdateListener()
-      .subscribe((posts: Post[]) => {
+      .subscribe((postData: {posts: Post[], postCount: number}) => {
         this.isLoading = false;
-        this.posts = posts;
+        this.totalPosts = postData.postCount;
+        this.posts = postData.posts;
       })
   }
+  onChangedPage(pageData: PageEvent) {
+    this.isLoading = true;
+    this.currentPage = pageData.pageIndex + 1;
+    this.postsPerPage = pageData.pageSize;
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
+  }
+
   onDelete(postId: string){
-    this.postsService.deletePost(postId);
+    this.isLoading = true;
+    this.postsService.deletePost(postId).subscribe(() => {
+      this.postsService.getPosts(this.postsPerPage, this.currentPage)
+    });
   }
 
   ngOnDestroy() {

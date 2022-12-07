4dccf10275ab72f0ee20831221f454abd76acae4
fluffyhake <haakon.reppen@gmail.com>
2022-12-07 15:16:17 +0100

    Add the signup component and add routing for it

--- mean-course/src/app/app-routing.module.ts
+++ mean-course/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { NgModel } from "@angular/forms";
 import { RouterModule, Routes } from "@angular/router"
 import { LoginComponent } from "./auth/login/login.component";
+import { SignupComponent } from "./auth/signup/signup.component";
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { PostListComponent } from "./posts/post-list/post-list.component";
 
@@ -12,7 +13,8 @@ const routes: Routes = [
   { path: 'create', component: PostCreateComponent},
   // /: makes the path dynamic, the word after ":" defines what variable name will be assigned to the uri value
   { path: 'edit/:postId', component: PostCreateComponent},
-  { path: 'login', component: LoginComponent}
+  { path: 'login', component: LoginComponent},
+  { path: 'signup', component: SignupComponent }
 ]
 
 @NgModule({

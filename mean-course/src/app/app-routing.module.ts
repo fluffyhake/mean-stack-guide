a4eb34a5ebf9f1c3d904f6be076cc11f820d6f35
fluffyhake <haakon.reppen@gmail.com>
2022-12-07 12:04:37 +0100

    add import of the login component, create paths for the login component to make it available

--- mean-course/src/app/app-routing.module.ts
+++ mean-course/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { NgModel } from "@angular/forms";
 import { RouterModule, Routes } from "@angular/router"
+import { LoginComponent } from "./auth/login/login.component";
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { PostListComponent } from "./posts/post-list/post-list.component";
 
@@ -10,7 +11,8 @@ const routes: Routes = [
   { path: '', component: PostListComponent},
   { path: 'create', component: PostCreateComponent},
   // /: makes the path dynamic, the word after ":" defines what variable name will be assigned to the uri value
-  { path: 'edit/:postId', component: PostCreateComponent}
+  { path: 'edit/:postId', component: PostCreateComponent},
+  { path: 'login', component: LoginComponent}
 ]
 
 @NgModule({

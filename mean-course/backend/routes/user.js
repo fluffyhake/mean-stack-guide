170bfb19a7d782c50b93401bb4afe37b03457a11
fluffyhake <haakon.reppen@gmail.com>
2022-12-13 10:06:52 +0100

    Add backend logic for authenticating the user. Compare the hash of password to the one in db with bcrypt

--- mean-course/backend/routes/user.js
+++ mean-course/backend/routes/user.js
@@ -2,9 +2,11 @@ const express = require("express");
 
 // Import the hashing plugin
 const bcrypt = require("bcrypt")
+// import package to create webtokens
+const jwt = require("jsonwebtoken")
 
 // Import the user template
-const User = require("../models/user")
+const User = require("../models/user");
 
 
 const router = express.Router()
@@ -35,4 +37,50 @@ router.post("/signup", (req, res, next) => {
 
 
 });
+
+router.post("/login", (req, res, next) => {
+  let fetchedUser;
+  console.log("Inside function for /login")
+  // Finds a user in the database
+  User.findOne({ email: req.body.email}).then(user => {
+    console.log("This is the user returned from mongo " + user)
+    if (!user) {
+      console.log("Inside the if !user")
+      return res.status(401).json({
+        message: "Auth failed"
+      });
+    }
+    fetchedUser = user
+    // Compare imput to an encrypted value
+    return bcrypt.compare(req.body.password, user.password);
+
+    })
+    .then(result => {
+      console.log(fetchedUser.email)
+      console.log(fetchedUser._id)
+      if (!result) {
+        return res.status(401).json({
+          message: "Auth failed"
+        })
+      }
+      const token = jwt.sign({email: fetchedUser.email, userId: fetchedUser._id},
+        'secret_this_should_be_longer',
+        {expiresIn: '1h'}
+      );
+      console.log(token)
+      res.status(201).json({
+        message: "login success",
+        token: token
+      })
+    })
+    .catch(err => {
+      return res.status(401).json({
+        message: "Auth failed - all error catch"
+      })
+    })
+
+
+
+})
+
 module.exports = router

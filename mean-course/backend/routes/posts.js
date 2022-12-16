0d634fc6db94b5d97e4e1e7485930ead6505b144
fluffyhake <haakon.reppen@gmail.com>
2022-12-16 12:19:29 +0100

    Import the checkauth middleware and use it on protected paths

--- mean-course/backend/routes/posts.js
+++ mean-course/backend/routes/posts.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const Post = require("../models/post")
+
+const checkAuth = require("../middleware/check-auth")
+
 const multer = require("multer");
 const { create } = require("../models/post");
 
@@ -30,7 +33,11 @@ const storage = multer.diskStorage({
 
 });
 
-router.post("", multer({storage: storage}).single("image"), (req,res, next) => {
+router.post(
+  "",
+  checkAuth,
+  multer({storage: storage}).single("image"),
+  (req,res, next) => {
   const url = req.protocol + '://' + req.get("host");
   const post = new Post({
     title: req.body.title,
@@ -89,7 +96,11 @@ router.get('', (req, res, next) => {
 
 });
 
-router.put("/:id", multer({storage: storage}).single("image"), (req, res, next) => {
+router.put("/:id",
+// Note: We don't need to call the function with the parameters. Express will take care of that for you :) and pass along the request data
+  checkAuth,
+  multer({storage: storage}).single("image"),
+  (req, res, next) => {
   let imagePath = req.body.imagePath;
   if (req.file) {
     const url = req.protocol + '://' + req.get("host");
@@ -120,7 +131,7 @@ router.get("/:id", (req, res, next) => {
 })
 
 // ALLOW Paths with ID-s after posts when deleting:
-router.delete("/:id", (req, res, next) =>{
+router.delete("/:id", checkAuth, (req, res, next) =>{
 
   console.log(req.params.id);
   Post.deleteOne({_id: req.params.id}).then(result => {

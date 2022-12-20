1c063851631899ddbe56f35521885ee1eee517ee
fluffyhake <haakon.reppen@gmail.com>
2022-12-20 08:23:58 +0100

    Add redirects, and add a logout function.

--- mean-course/src/app/auth/auth.service.ts
+++ mean-course/src/app/auth/auth.service.ts
@@ -1,20 +1,33 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { Subject } from "rxjs";
 import { AuthData } from "./auth-data.model";
 
 @Injectable({
   providedIn: "root"
 })
 export class AuthService{
+  private isAuthenticated = false
   private token: string;
+  private authStatusListener = new Subject<boolean>();
   // What does this do..? inits with the httpclient? Makes the httpclient available?
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
 
   getToken(){
     return this.token;
   }
 
+  getIsAuth(){
+    return this.isAuthenticated;
+  }
+
+  getAuthStatusListener() {
+    // Any part of the app can subscribe to this observable and get notified when the function is called
+    // The function will be called with either true or false to inform everyone wether the user is authenticated or not
+    return this.authStatusListener.asObservable();
+  }
 
   createUser(email: string, password: string) {
     const authData: AuthData = {email: email, password: password}
@@ -31,6 +44,17 @@ export class AuthService{
       .subscribe(response => {
         const token = response.token;
         this.token = token;
+        if (token) {
+          this.isAuthenticated = true;
+          this.authStatusListener.next(true);
+          this.router.navigate(['/'])
+        }
       })
+  }
+  logout(){
+    this.token = null;
+    this.isAuthenticated = false
+    this.authStatusListener.next(false)
+    this.router.navigate(['/'])
   }
 }

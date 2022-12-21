5882b86f534b1e7b84ab356442f6d0b2d25e07e0
fluffyhake <haakon.reppen@gmail.com>
2022-12-21 15:40:08 +0100

    add countdown and remove session when the timout countdown hits zero

--- mean-course/src/app/auth/auth.service.ts
+++ mean-course/src/app/auth/auth.service.ts
@@ -10,6 +10,7 @@ import { AuthData } from "./auth-data.model";
 export class AuthService{
   private isAuthenticated = false
   private token: string;
+  private tokenTimer: NodeJS.Timer;
   private authStatusListener = new Subject<boolean>();
   // What does this do..? inits with the httpclient? Makes the httpclient available?
   constructor(private http: HttpClient, private router: Router) {}
@@ -40,11 +41,16 @@ export class AuthService{
   login(email: string, password: string) {
     const authData: AuthData = {email: email, password: password}
 
-    this.http.post<{token: string}>("http://localhost:3000/api/user/login", authData)
+    this.http.post<{token: string, expiresIn: number}>("http://localhost:3000/api/user/login", authData)
       .subscribe(response => {
         const token = response.token;
         this.token = token;
         if (token) {
+          const expiresInDuration = response.expiresIn
+          this.tokenTimer = setTimeout(() => {
+            this.logout();
+          }, expiresInDuration * 1000)
+          console.log(expiresInDuration)
           this.isAuthenticated = true;
           this.authStatusListener.next(true);
           this.router.navigate(['/'])
@@ -53,8 +59,9 @@ export class AuthService{
   }
   logout(){
     this.token = null;
-    this.isAuthenticated = false
-    this.authStatusListener.next(false)
-    this.router.navigate(['/'])
+    this.isAuthenticated = false;
+    this.authStatusListener.next(false);
+    this.router.navigate(['/']);
+    clearTimeout(this.tokenTimer);
   }
 }

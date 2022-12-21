2ea155eb1eae4ac025a0d8466a0f6707d8d04ae3
fluffyhake <haakon.reppen@gmail.com>
2022-12-21 15:28:27 +0100

    add the frontend route-guard and protect routes that require authentication

--- mean-course/src/app/app-routing.module.ts
+++ mean-course/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { NgModel } from "@angular/forms";
 import { RouterModule, Routes } from "@angular/router"
+import { AuthGuard } from "./auth/auth.guard";
 import { LoginComponent } from "./auth/login/login.component";
 import { SignupComponent } from "./auth/signup/signup.component";
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
@@ -10,9 +11,9 @@ import { PostListComponent } from "./posts/post-list/post-list.component";
 // Routes is an array of javascript objects
 const routes: Routes = [
   { path: '', component: PostListComponent},
-  { path: 'create', component: PostCreateComponent},
+  { path: 'create', component: PostCreateComponent, canActivate: [AuthGuard]},
   // /: makes the path dynamic, the word after ":" defines what variable name will be assigned to the uri value
-  { path: 'edit/:postId', component: PostCreateComponent},
+  { path: 'edit/:postId', component: PostCreateComponent, canActivate: [AuthGuard]},
   { path: 'login', component: LoginComponent},
   { path: 'signup', component: SignupComponent }
 ]
@@ -20,7 +21,8 @@ const routes: Routes = [
 @NgModule({
   // Import routes to angular
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 
 
 })

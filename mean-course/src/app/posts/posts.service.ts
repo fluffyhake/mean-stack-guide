b2552b5be761d1dcf89dce9257c1f03ec64740fc
fluffyhake <haakon.reppen@gmail.com>
2022-12-30 11:31:52 +0100

    Add the creator field to the post class

--- mean-course/src/app/posts/posts.service.ts
+++ mean-course/src/app/posts/posts.service.ts
@@ -32,7 +32,8 @@ export class PostsService {
             title: post.title,
             content: post.content,
             id: post._id,
-            imagePath: post.imagePath
+            imagePath: post.imagePath,
+            creator: post.creator
           }
         }), maxPosts: postData.maxPosts
       };
@@ -40,6 +41,7 @@ export class PostsService {
 
       }))
       .subscribe((transformedPostData) => {
+        console.log(transformedPostData)
         this.posts = transformedPostData.posts;
         this.postsUpdated.next({
           posts: [...this.posts],

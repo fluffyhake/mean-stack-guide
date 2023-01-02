43c77aef5cc4a1040b218d026a50608318662408
fluffyhake <haakon.reppen@gmail.com>
2023-01-02 08:42:09 +0100

    Add userId so we can check user

--- mean-course/src/app/posts/post-list/post-list.component.ts
+++ mean-course/src/app/posts/post-list/post-list.component.ts
@@ -25,6 +25,7 @@ export class PostListComponent implements OnInit, OnDestroy {
   currentPage = 1;
   pageSizeOptions = [1, 2, 5, 10];
   userIsAuthenticated = false;
+  userId: string;
 
   posts: Post[] = [];
   private postsSub: Subscription;
@@ -35,6 +36,7 @@ export class PostListComponent implements OnInit, OnDestroy {
   ngOnInit(){
     this.isLoading = true;
     this.postsService.getPosts(this.postsPerPage, this.currentPage);
+    this.userId = this.AuthService.getUserId();
     this.postsSub = this.postsService.getPostUpdateListener()
       .subscribe((postData: {posts: Post[], postCount: number}) => {
         this.isLoading = false;
@@ -46,6 +48,7 @@ export class PostListComponent implements OnInit, OnDestroy {
         .getAuthStatusListener()
         .subscribe(isAuthenticated => {
           this.userIsAuthenticated = isAuthenticated;
+          this.userId = this.AuthService.getUserId();
         });
   }
   onChangedPage(pageData: PageEvent) {

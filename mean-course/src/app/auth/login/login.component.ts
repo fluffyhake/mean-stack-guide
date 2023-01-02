6a5dfff57144c52c32fe6c42fe14bfa8e74a56cb
fluffyhake <haakon.reppen@gmail.com>
2023-01-02 10:54:56 +0100

    create the authstatussub and update it

--- mean-course/src/app/auth/login/login.component.ts
+++ mean-course/src/app/auth/login/login.component.ts
@@ -1,17 +1,26 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { AuthService } from "../auth.service";
+import { Subscription } from "rxjs";
 
 
 @Component({
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent{
+export class LoginComponent implements OnInit, OnDestroy{
   isLoading = false
-
+  private authStatusSub: Subscription
 
   constructor(public authService: AuthService) {}
+
+  ngOnInit() {
+    this.authStatusSub = this.authService.getAuthStatusListener().subscribe(
+      authStatus => {
+        this.isLoading = false
+      }
+    );
+  }
 
   onLogin(form: NgForm) {
     if (form.invalid) {
@@ -21,6 +30,12 @@ export class LoginComponent{
     this.authService.login(form.value.email, form.value.password);
     console.log(form.value)
   }
+  ngOnDestroy(){
+    this.authStatusSub.unsubscribe
+  }
+
+
+
 }
 
 
